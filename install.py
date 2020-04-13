f2cb43a68adab1eb46e07abcce1870bd998aec86
SKILT08 <55826543+SKILT08@users.noreply.github.com>
2020-04-14 00:20:02 +0300

    Update install.py

--- install.py
+++ install.py
@@ -3,6 +3,6 @@ import os
 import sys
 print "\033[32mCreated by Artyom(c4md/SKILT)"
 print "Installing..."
-os.system("apt install python3.8 && chmod 777 ./kerneldev && dos2unix kerneldev")
+os.system("apt install python3.8 && chmod 777 ./kerneldev && apt install dos2unix && dos2unix kerneldev")
 print "\033[31mDone!"
 sys.exit()

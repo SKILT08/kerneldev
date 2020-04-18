215215fad8564c74114176f7d26c401064bff307
SKILT08 <55826543+SKILT08@users.noreply.github.com>
2020-04-18 15:21:22 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python2
+import os
+import sys
+print "\033[32mCreated by Artyom(c4md/SKILT)"
+print "Installing..."
+os.system("apt install python3.8 && chmod 777 ./kerneldev && dos2unix kerneldev")
+print "\033[31mDone!"
+sys.exit()

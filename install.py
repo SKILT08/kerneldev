745a90c33ca554d1701f26c27a21bf43deced5a3
SKILT08 <55826543+SKILT08@users.noreply.github.com>
2020-04-18 14:40:41 +0300

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-import os
-import sys
-print "\033[32mCreated by Artyom(c4md/SKILT)"
-print "Installing..."
-os.system("apt install python3.8 && chmod 777 ./kerneldev && apt install dos2unix && dos2unix kerneldev")
-print "\033[31mDone!"
-sys.exit()

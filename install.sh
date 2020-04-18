29a69e13ef62d3f1d8106161ab629e9a5f69aa3f
SKILT08 <55826543+SKILT08@users.noreply.github.com>
2020-04-18 14:54:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! bin/bash
 echo -e "\033[32mCreated by Artyom(c4md/SKILT)"
 echo -e "Installing..."
-apt install python3.8 && chmod 777 ./kerneldev && && chmod 777 install.sh && chmod 777 ./install.sh && chmod 777 kerneldev && sed -i -e 's/\r$//' kerneldev
+apt install python3.8 && chmod 777 ./kerneldev && sed -i -e 's/\r$//' kerneldev
 echo -e "\033[31mDone!"

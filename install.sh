8f2d9e4efccb4d51be356b2a7136f33349b41f8f
SKILT08 <55826543+SKILT08@users.noreply.github.com>
2020-04-18 15:20:53 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#! bin/bash
-echo -e "\033[32mCreated by Artyom(c4md/SKILT)"
-echo -e "Installing..."
-apt install python3.8 && chmod 777 ./kerneldev && sed -i -e 's/\r$//' kerneldev
-echo -e "\033[31mDone!"

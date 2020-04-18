694dd79490f5a939d55184df0ba129e7b8a11fc1
SKILT08 <55826543+SKILT08@users.noreply.github.com>
2020-04-18 14:45:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! bin/bash
 echo -e "\033[32mCreated by Artyom(c4md/SKILT)"
 echo -e "Installing..."
-apt install python3.8 && chmod 777 ./kerneldev && dos2unix kerneldev
+apt install python3.8
+chmod 777 ./kerneldev
+apt autoremove dos2unix
+apt install dos2unix
+dos2unix kerneldev
 echo -e "\033[31mDone!"
